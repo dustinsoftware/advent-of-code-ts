785e996860631a0524db27a3af4bf534e4cb09d0
Dustin Masters <dustin.masters@olo.com>
2019-12-01 23:57:34 -0500

    Day 1 part 1

--- day1.spec.ts
+++ day1.spec.ts
@@ -1,5 +1,13 @@
-import { A } from './day1';
+import { fuel, getAllFuel } from './day1';
 
 test('should work', () => {
-    expect(A()).toBe(2);
+    expect(fuel(12)).toBe(2);
+    expect(fuel(14)).toBe(2);
+    expect(fuel((1969))).toBe(654);
+    expect(fuel((100756))).toBe(33583);
+});
+
+test('summarize fuel', () => {
+    expect(getAllFuel(`12
+    14`)).toBe(4);
 });
--- day1.ts
+++ day1.ts
@@ -1,3 +1,10 @@
-export function A() {
-    return 2;
+export function fuel(mass: number) {
+    return Math.floor(mass / 3) - 2;
 }
+
+export const getAllFuel = (input: string) =>
+    input.split('\n')
+    .map(Number)
+    .filter(Boolean)
+    .map(A)
+    .reduce((prev, curr) => prev + curr, 0)
