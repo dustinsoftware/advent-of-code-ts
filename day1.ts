ff7e72a1d8cc4ce75392ce9dd5d70c27d99052c9
Dustin Masters <dustin.masters@olo.com>
2019-12-02 00:08:53 -0500

    Day 1 part 2

--- day1.ts
+++ day1.ts
@@ -6,5 +6,27 @@ export const getAllFuel = (input: string) =>
     input.split('\n')
     .map(Number)
     .filter(Boolean)
-    .map(A)
+    .map(fuel)
+    .reduce((prev, curr) => prev + curr, 0)
+
+export const expensiveFuel = (mass: number) => {
+    let next = mass;
+    let accumulator = 0;
+    while (true) {
+        let result = fuel(next);
+        next = result;
+        if (result <= 0) {
+            break;
+        }
+        accumulator += result;
+    }
+
+    return accumulator;
+}
+
+export const getAllExpensiveFuel = (input: string) =>
+    input.split('\n')
+    .map(Number)
+    .filter(Boolean)
+    .map(expensiveFuel)
     .reduce((prev, curr) => prev + curr, 0)
