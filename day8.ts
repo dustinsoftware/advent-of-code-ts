3b7597887e681b1024b2aedb89df8373215ea70f
Dustin Masters <dustin.masters@olo.com>
2019-12-10 22:02:05 -0500

    Day 8 part 2

--- day8.ts
+++ day8.ts
@@ -18,6 +18,39 @@ export function parseGrid(grid: string, columns: number, rows: number): XY[] {
         }));
 }
 
+export function mergeLayers(grid: XY[]): string {
+    let layerSize = grid
+        .map(x => (x.X + 1) * (x.Y + 1))
+        .sort((a, b) => b - a)[0];
+
+    let columnSize = grid
+        .map(x => x.X)
+        .sort((a, b) => b - a)[0] + 1;
+
+    // sigh, another layer swapping
+    let totalLayers = grid.length / layerSize;
+    let layeredGrid: XY[][] = Array(totalLayers).fill(0).map(() => []);
+
+    for (let point in grid) {
+        let layerNumber = Math.floor(Number(point) / layerSize);
+        let layer = layeredGrid[totalLayers - layerNumber - 1];
+        layer[Number(point) % layerSize] = grid[point];
+    }
+
+    let finalLayer = layeredGrid[0].map(x => x.value === 0 ? ' ' : 'B');
+    for (let layer of layeredGrid) {
+        for (let point in layer) {
+            finalLayer[point] = layer[point].value === 0 ? ' ' : layer[point].value === 1 ? 'B' : finalLayer[point];
+        }
+    }
+
+    let layerAsString: string = finalLayer.map((value, i) => i % columnSize === 0 ? value + '\n' : value).join('');
+
+    // a bit lazy to fix this up
+    console.log(layerAsString);
+    return '';
+}
+
 export function findFewestZerosLayer(grid: XY[]): XY[] {
     let layerSize = grid
         .map(x => (x.X + 1) * (x.Y + 1))
@@ -34,12 +67,12 @@ export function findFewestZerosLayer(grid: XY[]): XY[] {
         }
     }
 
-
     return Array(...layerMap)
         .map(x => x[1])
         .sort(
             (a, b) =>
-                a.filter(x => x.value === 0).length - b.filter(x => x.value === 0).length
+                a.filter(x => x.value === 0).length -
+                b.filter(x => x.value === 0).length
         )[0];
 }
 
