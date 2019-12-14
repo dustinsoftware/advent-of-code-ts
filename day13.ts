9466f3be72a46b4a014a18c2b22530c772b991fd
Dustin Masters <dustin.masters@olo.com>
2019-12-14 00:27:28 -0500

    Day 13 part 2

--- day13.ts
+++ day13.ts
@@ -201,16 +201,59 @@ export function drawGame(outputs: number[]): XY[] {
     return grid;
 }
 
+export function playGame(program: string): XY[] {
+    let grid: XY[] = [];
+    let iterations = 0;
+    let instance = runOpcodes(program);
+    let outputs: number[] = [];
+    let initialGameRun = true;
+
+    let nextInput = undefined;
+    while (true) {
+        if (iterations++ > 100000) {
+            throw new Error('Logic error, infinite loop');
+        }
+        let nextOutput = instance.next(nextInput);
+        nextInput = undefined;
+
+        if (nextOutput.value.instruction === 3) {
+            initialGameRun = false;
+
+            let ball = grid.filter(x => x.value === 3)[0];
+            let paddle = grid.filter(x => x.value === 4)[0];
+
+            if (paddle.X < ball.X) {
+                nextInput = -1;
+            } else if (paddle.X > ball.X) {
+                nextInput = 1;
+            } else {
+                nextInput = 0; // neutral position
+            }
+        } else if (nextOutput.value.instruction === 4) {
+            if (nextOutput.value.output !== undefined) {
+                outputs.push(nextOutput.value.output);
+                if (outputs.length === 3) {
+                    let [x, y, tileId] = outputs;
+                    outputs = [];
+                    getCell({ X: x, Y: y }, grid).value = tileId;
+                }
+            }
+        } else if (nextOutput.value.instruction === 99) {
+            break;
+        }
+    }
+
+    return grid;
+}
+
 export type XY = {
     X: number;
     Y: number;
     value: number;
 };
 
-export let getCell = (XY: { X: number, Y: number }, cells: XY[]): XY => {
-    let cell = cells.filter(
-        cell => cell.X === XY.X && cell.Y === XY.Y
-    )[0];
+export let getCell = (XY: { X: number; Y: number }, cells: XY[]): XY => {
+    let cell = cells.filter(cell => cell.X === XY.X && cell.Y === XY.Y)[0];
 
     if (!cell) {
         cell = { X: XY.X, Y: XY.Y, value: 0 };
